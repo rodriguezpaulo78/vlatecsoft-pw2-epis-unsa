4aa4d76b88dc823e913be48276f2dacfe721cb87
danilocr <crucesdanilo@gmail.com>
2016-05-31 00:10:27 -0500

    Agregar

---
+++ Prueba.java
@@ -0,0 +1,14 @@
+package asd;
+
+public class Prueba {
+	public static void main(String [] args) {
+		int i=3;
+		System.out.print(holas(i));
+		System.out.print(i);
+	}
+	public static int holas(int a){
+		int k=6;
+		a++;
+		return k;
+	}
+}
